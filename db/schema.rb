d369fecd6c9f92ed0ff88e9c5ac858a39083ad3d
Nenad Nikolic <nnikolic87@gmail.com>
2014-09-22 12:41:53 +0200

    adding crew model

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140922104026) do
+
+  create_table "crews", force: true do |t|
+    t.integer  "age"
+    t.string   "name"
+    t.string   "avatar"
+    t.string   "title"
+    t.string   "species"
+    t.string   "origin"
+    t.text     "quote"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
